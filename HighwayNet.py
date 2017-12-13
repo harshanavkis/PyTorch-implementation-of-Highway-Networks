e7825baa956617d2ef979a60fcd85fb34e0e8f7c
harshanavkis <harshanavkis@gmail.com>
2017-12-13 22:13:56 +0530

    Implement Highway Fully connected net

--- HighwayNet.py
+++ HighwayNet.py
@@ -17,16 +17,43 @@ class FcNet(nn.Module):
 		# self.layers = {}
 		self.numLayers = numLayers
 		for i in range(numLayers):
-			w = "layer"+str(i+1)
-			vars(self)[w] = torch.nn.Linear(dimArr[i],dimArr[i+1])
+			w = "layer" + str(i+1)
+			vars(self)[w] = nn.Linear(dimArr[i],dimArr[i+1])
 
 	def forward(self,x):
 		"""
 			Accept an input variable and produce the output
 		"""
 		numLayers = self.numLayers
-		for i in range(numLayers):
+		for i in range(numLayers-1):
 			w = "layer"+str(i+1)
 			x = F.relu(vars(self)[w](x))
-		w = "layer"+str(numLayers-1)
+		w = "layer"+str(numLayers)
 		return vars(self)[w](x)
+
+
+class HighwayFcNet(nn.Module):
+	def __init__(self, size, numLayers):
+		super(HighwayFcNet,self).__init__()
+		self.numLayers = numLayers
+		for i in range(numLayers):
+			wh = "wh" + str(i+1)
+			wt = "wt" + str(i+1)
+			# wc = "wc" + str(i+1)
+			vars(self)[wh] = nn.Linear(size,size)
+			vars(self)[wt] = nn.Linear(size,size)
+			# vars(self)[wc] = nn.sub(1.0,vars(self)[t])
+
+	def forward(self,x):
+		numLayers = self.numLayers
+		for i in range(numLayers-1):
+			wh = "wh" + str(i+1)
+			wt = "wt" + str(i+1)
+			# wc = "wc" + str(i+1)
+			h = F.relu(vars(self)[wh](x))
+			t = F.sigmoid(vars(self)[wt](x))
+			c = 1.0 - t
+			x = torch.add(torch.mul(h,t),torch.mul(x,c))
+		wh = "wh" + str(numLayers)
+		return vars(self)[wh](x)
+		return x
