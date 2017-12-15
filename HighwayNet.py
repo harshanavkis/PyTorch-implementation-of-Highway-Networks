minor fixes; use softmax from nn.functional

HighwayNet.py
@@ -15,7 +15,7 @@ class FcNet(nn.Module):
 		super(FcNet,self).__init__()
 		# self.layers = {}
 		self.activation = gA(activation_type)
-		self.plain = nn.Linear(input_size,input_size)
+		self.plain = nn.Linear(input_size,output_size)
 
 	def forward(self,x):
 		"""

models.py
@@ -1,19 +1,20 @@
 import HighwayNet
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class FcModel(nn.Module):
 	def __init__(self, input_size,output_size, numLayers, hiddenDimArr, activation = 'ReLU'):
 		super(FcModel,self).__init__()
 		self.linears = nn.ModuleList([HighwayNet.FcNet(input_size,hiddenDimArr[0],activation)])
-		self.linears.extend([HighwayNet.FcNet(dimArr[i-1],dimArr[i],activation) for i in range(1,numLayers-1)])
-		self.append(nn.Linear(hiddenDimArr[-1],output_size))
+		self.linears.extend([HighwayNet.FcNet(hiddenDimArr[i-1],hiddenDimArr[i],activation) for i in range(1,numLayers)])
+		self.linears.append(nn.Linear(hiddenDimArr[-1],output_size))
 
 	def forward(self,x):
-		for l in linears:
+		for l in self.linears:
 			x = l(x)
-		x = nn.Softmax(x)
+		x = F.softmax(x)
 		return x
 
 class HighwayFcModel(nn.Module):
@@ -25,5 +26,5 @@ class HighwayFcModel(nn.Module):
 	def forward(self,x):
 		for h in self.highways:
 			x = h(x)
-		x = nn.Softmax(self.linear(x))
+		x = F.softmax(self.linear(x))
 		return x 